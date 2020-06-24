71f4e279d0966330b46378e753d29e5e7045062f
Fred Blanc <frederic.blanc@new-immo-group.com>
2020-06-24 12:07:06 +0200

    Simplifier classes

--- src/Repository.php
+++ src/Repository.php
@@ -4,6 +4,14 @@ namespace App;
 
 class Repository
 {
+    /** @var string */
+    protected $path_biens = 'data/biens.csv';
+
+    public function setPath(string $path)
+    {
+        $this->path_biens = $path;
+    }
+    
     /** @return \Iterable */
     public function findAll()
     {
