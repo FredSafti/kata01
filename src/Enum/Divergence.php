49772ebf5d59c33ef82fffa491e02b38e925e94c
Bastien ROUGÉ <bastien.rouge@new-immo-group.com>
2020-06-25 12:00:47 +0200

    Petite refacto

--- src/Enum/Divergence.php
+++ src/Enum/Divergence.php
@@ -7,4 +7,16 @@ class Divergence
     public const SAFTI = 'SAFTI';
     public const MEGAGENCE = 'megAgence';
     public const SAFTI_ES = 'SAFTI-ES';
+
+    public const BIEN_SOURCE = [
+        self::SAFTI => [
+            'web' => 'http://omega.dev.safti.local/Git/Frederic/kata01-api/biens.csv'
+        ],
+        self::MEGAGENCE => [
+            'web' => 'http://megasoft.dev.safti.local/Git/Frederic/kata01-api/megagence.csv'
+        ],
+        self::SAFTI_ES => [
+            'csv' => 'data/biens-ES.csv'
+        ]
+    ];
 }
--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -6,43 +6,42 @@ use App\Enum\Divergence;
 
 class Repository
 {
-    protected $path_biens = 'data/biens.csv';
     private $headers = array();
-
-    public function setPath($path)
-    {
-        $this->path_biens = $path;
-    }
     
     /** @return \Iterable */
     public function findAll()
     {
-        if ($this->divergence == Divergence::SAFTI_ES) {
-            $this->setPath('data/biens-ES.csv');
-        }
-        
-        switch($this->divergence)
-        {
-            case Divergence::SAFTI:
-            case Divergence::SAFTI_ES:
-                $biens = fopen($this->path_biens, 'r');
-                $result = array();
-                $this->headers = fgetcsv($biens, 0, ';');
-                while ($line = fgetcsv($biens, 0, ';'))
-                {
-                    $result[] = $this->transformLine($line);
-                }
-                return $result;
+        $dataSource = Divergence::BIEN_SOURCE[$this->divergence];
+        return isset($dataSource['csv'])
+            ? $this->getResultFromCsv($dataSource['csv'])
+            : $this->getResultFromWeb($dataSource['web']);
+    }
 
-            case Divergence::MEGAGENCE:
-                $content = file_get_contents('http://megasoft.dev.safti.local/Git/Frederic/kata01-api/megagence.csv');
-                $biens = explode("\n", $content);
-                $result = array();
-                foreach ($biens as $bien) {
-                    $result[] = $this->transformLine(explode(';', $bien));
-                }
-                return $result;
+    private function getResultFromCsv($path)
+    {
+        $biens = fopen($path, 'r');
+        $result = array();
+        $this->headers = fgetcsv($biens, 0, ';');
+        while ($line = fgetcsv($biens, 0, ';'))
+        {
+            $result[] = $this->transformLine($line);
         }
+        return $result;
+    }
+
+    private function getResultFromWeb($url)
+    {
+        $content = file_get_contents($url);
+        $biens = explode("\n", $content);
+
+        $this->headers = explode(';', $biens[0]);
+        unset($biens[0]);
+
+        $result = array();
+        foreach ($biens as $bien) {
+            $result[] = $this->transformLine(explode(';', $bien));
+        }
+        return $result;
     }
 
     private function transformLine($arr)
