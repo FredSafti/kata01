49316ffabd8344438f4362d71c7b5b1e696c5d31
Benoît Labadie <benoit.labadie@new-immo-group.com>
2020-06-25 12:03:29 +0200

    kata

--- src/Main.php
+++ src/Main.php
@@ -6,20 +6,6 @@ use App\Repository\Repository;
 
 class Main extends Repository
 {
-    /** @var string */
-    protected $divergence;
-
-    public function __construct($divergence)
-    {
-        $this->divergence = $divergence;
-    }
-
-    /** @return string */
-    public function getDivergence()
-    {
-        return $this->divergence;
-    }
-
     /** @return \Iterable */
     public function getListeBiens()
     {
--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -9,40 +9,69 @@ class Repository
     protected $path_biens = 'data/biens.csv';
     private $headers = array();
 
+    /** @var string */
+    protected $divergence;
+
+    public function __construct($divergence)
+    {
+        $this->divergence = $divergence;
+    }
+
+    /** @return string */
+    public function getDivergence()
+    {
+        return $this->divergence;
+    }
+
     public function setPath($path)
     {
         $this->path_biens = $path;
     }
-    
+
     /** @return \Iterable */
     public function findAll()
     {
-        if ($this->divergence == Divergence::SAFTI_ES) {
-            $this->setPath('data/biens-ES.csv');
-        }
-        
-        switch($this->divergence)
-        {
-            case Divergence::SAFTI:
+        switch ($this->divergence) {
             case Divergence::SAFTI_ES:
-                $biens = fopen($this->path_biens, 'r');
-                $result = array();
-                $this->headers = fgetcsv($biens, 0, ';');
-                while ($line = fgetcsv($biens, 0, ';'))
-                {
-                    $result[] = $this->transformLine($line);
-                }
-                return $result;
-
+                $this->setPath('data/biens-ES.csv');
+                break;
+            case Divergence::SAFTI:
+                $this->setPath('http://192.168.7.237:8081/Git/Frederic/kata01-api/biens.csv');
+                break;
             case Divergence::MEGAGENCE:
-                $content = file_get_contents('http://megasoft.dev.safti.local/Git/Frederic/kata01-api/megagence.csv');
-                $biens = explode("\n", $content);
-                $result = array();
-                foreach ($biens as $bien) {
-                    $result[] = $this->transformLine(explode(';', $bien));
-                }
-                return $result;
+                $this->setPath('http://192.168.7.237:8081/Git/Frederic/kata01-api/megagence.csv');
+                break;
         }
+
+        if(preg_match('/^https?/', $this->path_biens)) {
+            return $this->getContentDist();
+        } else {
+            return $this->getContentLocal();
+        }
+    }
+
+    private function getContentDist()
+    {
+        $content = file_get_contents($this->path_biens);
+        $biens = explode("\n", $content);
+        $this->headers = explode(';', array_shift($biens));
+        $result = array();
+        foreach ($biens as $bien) {
+            $result[] = $this->transformLine(explode(';', $bien));
+        }
+        return $result;
+    }
+
+
+    private function getContentLocal()
+    {
+        $biens = fopen($this->path_biens, 'r');
+        $result = array();
+        $this->headers = fgetcsv($biens, 0, ';');
+        while ($line = fgetcsv($biens, 0, ';')) {
+            $result[] = $this->transformLine($line);
+        }
+        return $result;
     }
 
     private function transformLine($arr)
